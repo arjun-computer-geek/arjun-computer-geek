cc8e37a76e962fd923e10c2f1496f568c9abd5b7
arjun <arjun2000raj@gmail.com>
2022-05-29 15:03:02 +0530

    Update reademe

---
+++ about.js
@@ -0,0 +1,16 @@
+console.log("hello world!");
+const arjunComputerGeek = {
+    pronouns: "he" | "his",
+    country: "India",
+    age: 22,
+    spokenLanguages: ["Hindi", "English"],
+    newYearGoals: ["sharpen my skills in programming",
+                   "Learn full stack development",
+                   "get a job"],
+    myFavourites :{
+        movies: "Iron Man",
+        character: "Tony Stark",
+        idealPerson: "Tanay Bhaiya",
+        books: "The Hitchhiker's Guide to the Galaxy",
+    }
+}
